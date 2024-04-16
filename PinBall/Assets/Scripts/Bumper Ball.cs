3bd3c937e0c85fbb5a645fba426bbff865ff3d27
Mak-Harrison <163163768+Mak-Harrison@users.noreply.github.com>
2024-04-16 15:40:20 -0400

    Bumper Ball Collision

---
+++ PinBall/Assets/Scripts/Bumper Ball.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperBall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int ballForce;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball")) 
+        {
+            rb.AddForce(Vector2.up * ballForce);
+        }
+    }
+}
