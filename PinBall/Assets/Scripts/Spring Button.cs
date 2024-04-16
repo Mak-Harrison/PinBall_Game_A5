79196fbe1e3792ff07cb4d32bf29abdb930243ac
Mak-Harrison <163163768+Mak-Harrison@users.noreply.github.com>
2024-04-15 22:11:55 -0400

    New Spring Code

--- PinBall/Assets/Scripts/Spring Button.cs
+++ PinBall/Assets/Scripts/Spring Button.cs
@@ -1,58 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class SpringButton : MonoBehaviour
 
 {
-    public int Points;
-    public GameObject Spring;
-    public float PowerPerSecond;
-    public int SpringStatus;
-    public float Power;
-    public bool Collider;
-    void Start()
-    {
-        // When Game Starts reset these values.
-        SpringStatus = 1;
-        Power = 0f;
-        Points = 0;
-        Spring = GameObject.Find("Spring");
-        Collider = true;
-    }
+    public float speed = 5;
 
-    void SpringGettingCompressed()
+    private void Start()
     {
-        if (transform.position.x > -2.3)
-        {
-            transform.position = new Vector3(transform.position.x - 0.05f * Time.deltaTime, transform.position.y, transform.position.z);
-            Power += PowerPerSecond * Time.deltaTime;
-
-        }
-    }
-
-    void SpringReleased()
-    {
-        if (transform.position.x <= -1.9)
-        {
-            transform.position = new Vector3(transform.position.x + Power * Time.deltaTime, transform.position.y, transform.position.z);
-            SpringStatus = 2;
-            Collider = false;
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If spring is in out position & user holds down the Space Button, Start Compressing the spring.
-        if (SpringStatus == 1 && Input.GetKey(KeyCode.Space))
-        {
-            SpringGettingCompressed();
-        }
+        Move();
+    }
 
-        if (Input.GetKeyUp(KeyCode.Space) && SpringStatus == 1)
-        {
-            SpringReleased();
-        }
+    void Move()
+    {
+        
     }
 }
